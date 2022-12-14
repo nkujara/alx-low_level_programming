919b6b22e59767fe9dad279f0d224e28f5650c04
nkujara <mhlangankululeko@yahoo.com>
2022-12-14 04:30:48 -0800

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * Return (0)
  */
 int main(void)
-
+{
 	_putchar(' ');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +17,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0)
-
+	return (0);
+}		
