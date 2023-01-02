ad78c1deab55d8dd0011f7970ebd6863c7aaef3c
nkujara <mhlangankululeko@yahoo.com>
2023-01-02 09:43:12 -0800

    kasdar

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -5,35 +5,31 @@
 
 /**
  *
- *	 simple_print_buffer - prints buffer in hexa
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- *	  @buffer: the address of memory to print
- *
- *	 @size: the size of the memory to print
- *
- *
- *
- *	 Return: Nothing.
+ * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(char buffer, unsigned int size)
 {
-		unsigned int i;
+	unsigned int i;
 
-		i = 0;
-		while (i < size)
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
 		{
-				if (i % 10)
-				{
-						printf(" ");
-				}
-				if (!(i % 10) && i)
-				{
-						printf("\n");
-				}
-				printf("0x%02x", buffer[i]);
-				i++;
+			printf(" ");
 		}
-		printf("\n");
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
@@ -43,11 +39,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-		 char buffer[98] = {0x00};
+	char buffer[98] = {0x00};
 
-		 simple_print_buffer(buffer, 98);
-		 _memset(buffer, 0x01, 95);
-		 printf("-------------------------------------------------\n");
-		 simple_print_buffer(buffer, 98);
-		 return (0);
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
