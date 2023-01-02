6f9e79d81d013acb425f6075cb9e1192cfc12076
nkujara <mhlangankululeko@yahoo.com>
2023-01-02 06:16:29 -0800

    kj

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,9 +10,8 @@
 
 #include <stddef.h>   
 
-int_putchar(char c);
+int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
-char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
